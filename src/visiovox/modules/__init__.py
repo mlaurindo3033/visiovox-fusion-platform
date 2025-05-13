cf1aa007d126b3dd5bf3f3e92b3a8d63f8d05588
mlaurindo3033 <95896688+mlaurindo3033@users.noreply.github.com>
2025-05-13 20:12:22 -0300

    Adicionar arquivos __init__.py para pacotes Python

---
+++ src/visiovox/modules/__init__.py
@@ -0,0 +1,3 @@
+# __init__.py for the visiovox.modules package
+# This package will host all the specialized functional modules of the VisioVox application,
+# including media ingestion, scene analysis, facial processing, voice processing, etc. 
---
+++ src/visiovox/modules/media_ingestion/__init__.py
@@ -0,0 +1,3 @@
+# __init__.py for the visiovox.modules.media_ingestion sub-package
+# This sub-package is responsible for loading and preparing media inputs
+# (images, videos, audio streams) for processing by other modules. 
